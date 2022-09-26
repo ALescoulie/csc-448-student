c69ca75e69be9ec0246baf90079bc9bd10efdace
Paul Anderson <pauleanderson@gmail.com>
2022-09-26 09:47:35 -0700

    update

---
+++ assignments/Assignment1_helper.py
@@ -0,0 +1,11 @@
+def read_fasta(file):
+    seqs = []
+    headers = []
+    # implement a read fasta file
+    # YOUR SOLUTION HERE
+    return headers,seqs
+
+
+def avg_length(sequences):
+    avg = None
+    return avg
